09a2459f2ab67836d658d020148d36c3e9f0cbe6
mteresa-jimenez <mteresa.jn@gmail.com>
2022-11-16 01:04:38 +0100

    Delete unused code

--- react-meetup/src/App.js
+++ react-meetup/src/App.js
@@ -4,13 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetupsPage";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
-import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./utils/constants";
+
 
 import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
 
 function App() {
-  const [page, setPage] = useState(ALL_MEETUP_PAGE);
   const [show, setShow] = useState(true);
   const [scrollY, setScrollY] = useState(0);
 
@@ -30,22 +29,6 @@ function App() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   });
-
-  function getCurrentPageComponent() {
-    let currentPageComponent = <AllMeetupsPage />;
-    switch (page) {
-      case FAVORITES_PAGE:
-        currentPageComponent = <FavoritesPage />;
-        break;
-      case NEW_MEETUP_PAGE:
-        currentPageComponent = <NewMeetupsPage />;
-        break;
-      default:
-        currentPageComponent = <AllMeetupsPage />;
-    }
-
-    return currentPageComponent;
-  }
 
   return (
     <div data-test="app">
